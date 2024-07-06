3c46caade640014b8e24986264aa4663243ee3e5
Kazuya INOUE <bauer24tbp@gmail.com>
2024-07-06 18:19:33 +0900

    Vue.jsとdaisyUIの環境構築まで

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="text-3xl text-center">Hello World!</h1>
+
+  <div class="card bg-base-100 w-96 shadow-xl">
+    <figure>
+      <img
+        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+        alt="Shoes" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+  </div>
+</template>
